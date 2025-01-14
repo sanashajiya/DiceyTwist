d3d2d625b1e2c98c871e1ef5fe9ed7fdd9bf5971
sanashajiya <ssanashajiya@gmail.com>
2025-01-15 01:23:36 +0530

    Added App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,21 @@ import 'package:dicey_twist/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: GradientContainer(
           Color.fromARGB(255, 65, 21, 146),
           Color.fromARGB(255, 65, 21, 146),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
